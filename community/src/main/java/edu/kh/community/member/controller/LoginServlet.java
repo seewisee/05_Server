aa999db490bafe0e173c21ec5bdf910b784baee2
seohyun <dhtjgus1356@naver.com>
2023-07-05 14:18:03 +0900

    7.5 수업내용

--- community/src/main/java/edu/kh/community/member/controller/LoginServlet.java
+++ community/src/main/java/edu/kh/community/member/controller/LoginServlet.java
@@ -8,6 +8,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import edu.kh.community.member.model.service.MemberService;
+import edu.kh.community.member.model.vo.Member;
+
 @WebServlet("/member/login")
 public class LoginServlet extends HttpServlet {
 
@@ -16,10 +19,11 @@ public class LoginServlet extends HttpServlet {
 
 		// POST 방식 요청 시 문자 인코딩이 서버 기본값으로 지정
 		// -> 한글 깨짐 -> 문자 인코딩 변경 필요
-		req.setCharacterEncoding("UTF-8");
+		//req.setCharacterEncoding("UTF-8");
 		
 		// * 모든 doPost() 메소드에 인코딩 변경 코드를 작성해야함.. (귀찮..)
-		
+		// -> 모든 요청 (전달 방식 가리지 않음) 시 req, resp의 문자 인코딩을 UTF-8로 변경
+		// -> 필터(Filter)
 		
 		
 		// 전달된 파라미터 변수에 저장
@@ -28,7 +32,31 @@ public class LoginServlet extends HttpServlet {
 		
 		System.out.println(inputEmail);
 		System.out.println(inputPw);
+		
+		
+		// 파라미터를 VO에 세팅(롬복 확인)
+		Member mem = new Member();
+		mem.setMemberEmail(inputEmail);
+		mem.setMemberPw(inputPw);
+		
+		try {
+			
+			// 서비스 객체 생성
+			MemberService service = new MemberService();
+			
+			// 이메일, 비밀번호가 일치하는 회원을 조회하는 서비스 호출 후 결과 반환 받기
+			Member loginMember = service.login(mem);
+			
+			System.out.println(loginMember);
+			
+			
+		}catch (Exception e) {
+			e.printStackTrace();
+			
+		}
+		
 	
+		
 	}
 	
 }
--- community/src/main/java/edu/kh/community/member/model/service/MemberService.java
+++ community/src/main/java/edu/kh/community/member/model/service/MemberService.java
@@ -1,5 +1,34 @@
 package edu.kh.community.member.model.service;
 
+import static edu.kh.community.common.JDBCTemplate.*;
+
+import java.sql.Connection;
+
+import edu.kh.community.member.model.dao.MemberDAO;
+import edu.kh.community.member.model.vo.Member;
+
 public class MemberService {
+
+	private MemberDAO dao = new MemberDAO();
+	
+	/** 로그인 서비스
+	 * @param mem
+	 * @return loginMember
+	 * @throws Exception 
+	 */
+	public Member login(Member mem) throws Exception {
+		
+		// connection 얻어오기
+		Connection conn = getConnection();
+		
+		// DAO 수행
+		Member loginMember = dao.login(conn, mem);
+		
+		// Connection 반환
+		close(conn);
+		
+		// 결과 반환
+		return loginMember;
+	}
 
 }
